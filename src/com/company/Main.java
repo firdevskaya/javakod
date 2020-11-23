8fa3f7fe008d693f12b208bfa83d6fe1cab7a6d1
firdevskaya <ffirdevskaya31@gmail.com>
2020-11-23 19:38:00 +0300

    message

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("firdevs kaya");
  System.out.println("locala gonder");
+        System.out.println("gite gonder");
+
+
     }
 }
